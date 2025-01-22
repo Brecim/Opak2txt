c4006d24ebe2349827f02a121b93fa9af89e6235
Brecim <reich_marek@oauh.cz>
2025-01-22 07:34:02 +0100

    nejak to upravit

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class FileManager {
@@ -8,29 +9,36 @@ public class FileManager {
 
 
     public void scan() {
-            try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("text.txt")))) {
-                while (scanner.hasNextLine()) {
-                    String radek = scanner.nextLine();
-                    String[] hodnoty = radek.split(";");
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("text.txt")))) {
+            while (scanner.hasNextLine()) {
+                String radek = scanner.nextLine();
+                String[] hodnoty = radek.split(";");
 
-                    for (String hodnota : hodnoty) {
-                        String[] idJmeno = hodnota.split(" ");
-                        int id = Integer.parseInt(idJmeno[0]);
-                        String jmeno = idJmeno[1];
+                for (String hodnota : hodnoty) {
+                    String[] idJmeno = hodnota.split(" ");
+                    int id = Integer.parseInt(idJmeno[0]);
+                    String jmeno = idJmeno[1];
+                    String prijmeni = idJmeno[2];
 
-                        if (jmeno.equals("Ivana") && id < 50) {
-                            System.out.println(id);
-                        }
+                    System.out.println(id + " " + jmeno);
+
+                    if (Objects.equals(jmeno, "Ivana")) {
+                        System.out.println("\nID zákazníků s jmenem Ivana: " + id);
                     }
+
+                    if (id < 50) {
+                        System.out.println("\nID zákazníků, které mají ID menší než 50: " + id);
+                    }
+
+
                 }
-                scanner.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("Soubor nebyl nalezen.");
-                System.exit(404);
-            } catch (IOException e) {
-                System.out.println("Chyba se souborem, soubor je možná poškozený.");
-                System.exit(100);
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Soubor nebyl nalezen.");
+            System.exit(404);
+        } catch (NumberFormatException e) {
+            System.out.println("Chybná číslová hodnota.");
+            System.exit(100);
         }
     }
 }
